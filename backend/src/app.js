4c60444951fe267774ce4927a1207c961db2758f
CoderXYZ14 <s1404islam@gmail.com>
2024-11-17 02:36:24 +0530

    backend-deployment-3

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,10 +30,13 @@ app.get("/", (req, res) => {
   res.json({ message: "API is working" });
 });
 app.use(express.json({ limit: "16kb" }));
-app.use(express.static("public"));
+import path from "path";
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
-
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/data", dataRouter);
 
