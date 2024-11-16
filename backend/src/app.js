bab1b88d1e0a6b89a3a71d124da39e88721d063f
CoderXYZ14 <s1404islam@gmail.com>
2024-11-17 02:16:20 +0530

    vercel.json added

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,10 +22,13 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
 );
-
+app.get("/", (req, res) => {
+  res.json({ message: "API is working" });
+});
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
