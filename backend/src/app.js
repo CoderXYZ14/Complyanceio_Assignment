3ac1a2e603198d2d2810cb0c2b27c3d941f0e039
CoderXYZ14 <s1404islam@gmail.com>
2024-11-16 16:45:39 +0530

    data routes setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import dataRouter from "./routes/data.routes.js";
+
 const app = express();
 
 app.use(
@@ -13,7 +16,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-import userRouter from "./routes/user.routes.js";
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/data", dataRouter);
 
 export { app };
