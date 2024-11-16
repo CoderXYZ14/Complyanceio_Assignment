544b381a82f9f160c99972c29128231679cb5f26
CoderXYZ14 <s1404islam@gmail.com>
2024-11-16 16:41:29 +0530

    data model added

---
+++ backend/src/models/data.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const dataSchema = new Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Data = mongoose.model("Data", dataSchema);
